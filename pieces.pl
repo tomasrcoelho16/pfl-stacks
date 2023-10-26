fd722c71f8a4d41a6690f6dac10c61759eda7f95
Carlos Costa <carloscosta.08062002@gmail.com>
2023-10-26 22:35:37 +0100

    i wanna die

---
+++ pieces.pl
@@ -0,0 +1,81 @@
+% Define a piece as a compound term with color (red or black) and stack size.
+% Example: red(2) represents a stack of 2 red pieces.
+% empty represents an empty cell.
+
+yo_bro:-
+    write('TOMASTOMASTOMAS').
+
+initial_state([
+    [empty, empty, empty, empty, empty],
+    [empty, empty, empty, empty, empty],
+    [empty, empty, empty, empty, empty],
+    [empty, empty, empty, empty, empty],
+    [empty, empty, empty, empty, empty],
+    [red(2), red(2), red(2), red(2), red(2)],
+    [black(2), black(2), black(2), black(2), black(2)]
+]).
+
+% Define a predicate to display the game board.
+display_board(Board) :-
+    nl,
+    display_columns,
+    display_separator,
+    display_rows(Board, 1).
+
+display_columns :-
+    write('   A   B   C   D   E\n').
+
+display_separator :-
+    write('  -------------------\n').
+
+display_rows([], 6).
+display_rows([Row | Rest], RowNumber) :-
+    display_separator,
+    write(RowNumber),
+    write(' '),
+    display_row(Row),
+    nl,
+    NextRowNumber is RowNumber + 1,
+    display_rows(Rest, NextRowNumber).
+
+display_row([]).
+display_row([Cell | Rest]) :-
+    write('| '),
+    display_cell(Cell),
+    display_row(Rest).
+
+display_cell(empty) :-
+    write(' ').
+display_cell(red(N)) :-
+    format('R~d', [N]).
+display_cell(black(N)) :-
+    format('B~d', [N]).
+
+
+
+% Move a piece from one column to another.
+move_piece([Row1|Rows], FromCol, ToCol, NewGameState) :-
+    valid_move(FromCol, ToCol),
+    select_stack(Row1, FromCol, Stack, RestRow),
+    piece_size(Stack, Size), % Get the size of the stack
+    % Update the stack size (if you are stacking or unstacking)
+    NewSize is Size - 1, % For unstacking
+    % NewSize is Size + 1, % For stacking
+    update_stack(Stack, NewSize, NewStack),
+    place_piece(RestRow, ToCol, NewStack, NewRow),
+    append([NewRow], Rows, NewGameState).
+
+% Select a stack from a row.
+select_stack(Row, Col, Stack, RestRow) :-
+    nth1(Col, Row, Stack),
+    select(Stack, Row, RestRow).
+
+% Get the size of the stack (0 if empty).
+piece_size(empty, 0).
+piece_size(red(Size), Size) :- Size > 0.
+piece_size(black(Size), Size) :- Size > 0.
+
+% Update the stack size.
+update_stack(empty, _, empty). % Clear the cell if unstacked completely.
+update_stack(red(Size), NewSize, NewStack) :- NewSize > 0, NewSize =< 4, NewStack = red(NewSize).
+update_stack(black(Size), NewSize, NewStack) :- NewSize > 0, NewSize =< 4, NewStack = black(NewSize).
--- stacks.pl
+++ stacks.pl
@@ -1,3 +1,70 @@
+% Load the predicates from pieces.pl
+
+
+% Define the main predicate to start the game.
+play :-
+    consult('pieces.pl'),
+    display_menu,
+    get_menu_option(Option),
+    handle_menu_option(Option).
+
+% Display the game menu.
+display_menu :-
+    write('Welcome to Stacks!\n'),
+    write('1. Human vs Human\n'),
+    write('2. Human vs Bot\n'),
+    write('3. Bot vs Bot\n'),
+    write('Select an option (1/2/3): ').
+
+% Get the user's menu option.
+get_menu_option(Option) :-
+    read(Option).
+
+% Handle the selected menu option.
+handle_menu_option(1) :- % Human vs Human
+    start_human_vs_human_game.
+handle_menu_option(2) :- % Human vs Bot
+    start_human_vs_bot_game.
+handle_menu_option(3) :- % Bot vs Bot
+    start_bot_vs_bot_game.
+handle_menu_option(_) :- % Handle invalid input
+    write('Invalid option. Please select a valid option.\n'),
+    play.
+
+% Define game modes as placeholders (you need to implement these).
+start_human_vs_human_game :-
+    % Initialize the game board (modify this based on your game logic).
+    write('yoo'),
+    yo_bro,
+    % initial_state(Board),
+
+    % Display the initial game board.
+    % display_board(Board),
+
+    % Implement the game loop here.
+    % Handle player moves, game rules, and so on.
+    % Display the updated game board in each iteration.
+
+    write('Human vs Human game logic goes here...\n').
+
+
+start_human_vs_bot_game :-
+    write('Starting Human vs Bot game...\n').
+
+start_bot_vs_bot_game :-
+    write('Starting Bot vs Bot game...\n').
+
+% Example of the main predicate to start the game.
+% You can modify this to fit your game's entry point.
+main :- play.
+
+
+
+
+
+
+
+
 board:-   write('|1b2|2b2|3b2|4b2|5b2|'),nl,
           write('---------------------'), nl,
           write('|   |   |   |   |   |'),nl,
